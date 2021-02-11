1f71a9b9601505bd5ded046a9f2e90bf957d8524
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-11 11:31:23 +0200

    Modifications to Makefile and additions of bash installation scripts
    To facilitate automatic installation of all necessary external packages, either individually or collectively.

---
+++ devtools/install_autotst.sh
@@ -0,0 +1,27 @@
+# temporarily change directory to install software, and move one directory up in the tree
+pushd ..
+
+# clone the repo in the parent directory
+echo "Cloning/Updating AutoTST..."
+git clone https://github.com/ReactionMechanismGenerator/AutoTST
+cd AutoTST || exit
+git fetch origin
+git checkout master
+git pull origin master
+
+# Add to PYTHONPATH
+echo "Adding AutoTST to PYTHONPATH..."
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+echo 'export PYTHONPATH=$PYTHONPATH:'"$(pwd)" >> ~/.bashrc
+
+# create the environment
+echo "Creating the AutoTST environment..."
+conda deactivate
+conda env create -f environment.yml
+conda activate tst_env
+conda install -c anaconda yaml
+conda deactivate
+
+# Restore the original directory
+echo "Done installing AutoTST."
+popd || exit
