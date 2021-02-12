8b842460946a1b3551d6f1302c99cc46451ec22b
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-13 01:15:15 +0200

    f gaussian

--- arc/job/adapters/gaussian.py
+++ arc/job/adapters/gaussian.py
@@ -209,8 +209,10 @@ class GaussianAdapter(JobAdapter):
         self.server = self.args['trsh']['server'] if 'server' in self.args['trsh'] \
             else self.ess_settings[self.job_adapter][0] if isinstance(self.ess_settings[self.job_adapter], list) \
             else self.ess_settings[self.job_adapter]
-        # self.species_label = self.species[0].label if self.species is not None else self.reaction.ts_label  # transfer to QST2
+        # self.species_label = self.species[0].label if self.species is not None else self.reaction.ts_label  # Todo: transfer to QST2
         self.species_label = self.species[0].label
+        if len(self.species) > 1:
+            self.species_label += f'_and_{len(self.species) - 1}_others'
 
         self.cpu_cores, self.input_file_memory, self.submit_script_memory = None, None, None
         self.set_cpu_and_mem()
