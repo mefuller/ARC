ae3bc99cad7705cbdc2191c9656bca55435484b9
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-13 00:38:24 +0200

    f gaussian
    f gaussian
    
    
    f gaussian

--- arc/job/adapters/gaussian.py
+++ arc/job/adapters/gaussian.py
@@ -68,7 +68,6 @@ class GaussianAdapter(JobAdapter):
         project (str): The project's name. Used for setting the remote path.
         project_directory (str): The path to the local project directory.
         job_type (list, str): The job's type, validated against ``JobTypeEnum``. If it's a list, pipe.py will be called.
-        level (Level): The level of theory to use.
         args (dict, optional): Methods (including troubleshooting) to be used in input files.
                                Keys are either 'keyword', 'block', or 'trsh', values are dictionaries with values
                                to be used either as keywords or as blocks in the respective software input file.
@@ -85,13 +84,14 @@ class GaussianAdapter(JobAdapter):
         ess_trsh_methods (List[str], optional): A list of troubleshooting methods already tried out.
         execution_type (str, optional): The execution type, 'incore', 'queue', or 'pipe'.
         fine (bool, optional): Whether to use fine geometry optimization parameters. Default: ``False``.
-        initial_time (datetime.datetime, optional): The time at which this job was initiated.
+        initial_time (datetime.datetime or str, optional): The time at which this job was initiated.
         irc_direction (str, optional): The direction of the IRC job (`forward` or `reverse`).
         job_id (int, optional): The job's ID determined by the server.
         job_memory_gb (int, optional): The total job allocated memory in GB (14 by default).
         job_name (str, optional): The job's name (e.g., 'opt_a103').
         job_num (int, optional): Used as the entry number in the database, as well as in ``job_name``.
         job_status (int, optional): The job's server and ESS statuses.
+        level (Level, optional): The level of theory to use.
         max_job_time (float, optional): The maximal allowed job time on the server in hours (can be fractional).
         reactions (List[ARCReaction], optional): Entries are ARCReaction instances, used for TS search methods.
         rotor_index (int, optional): The 0-indexed rotor number (key) in the species.rotors_dict dictionary.
@@ -99,7 +99,6 @@ class GaussianAdapter(JobAdapter):
         server_nodes (list, optional): The nodes this job was previously submitted to.
         species (List[ARCSpecies], optional): Entries are ARCSpecies instances.
                                               Either ``reactions`` or ``species`` must be given.
-        tasks (int, optional): The number of tasks to use in a job array (each task has several threads).
         testing (bool, optional): Whether the object is generated for testing purposes, ``True`` if it is.
         torsions (List[List[int]], optional): The 0-indexed atom indices of the torsions identifying this scan point.
         tsg (int, optional): TSGuess number if optimizing TS guesses.
@@ -109,8 +108,7 @@ class GaussianAdapter(JobAdapter):
     def __init__(self,
                  project: str,
                  project_directory: str,
-                 job_type: Optional[Union[List[str], str]] = None,
-                 level: Optional['Level'] = None,
+                 job_type: Union[List[str], str],
                  args: Optional[dict] = None,
                  bath_gas: Optional[str] = None,
                  checkfile: Optional[str] = None,
@@ -122,20 +120,20 @@ class GaussianAdapter(JobAdapter):
                  ess_trsh_methods: Optional[List[str]] = None,
                  execution_type: Optional[str] = None,
                  fine: bool = False,
-                 initial_time: Optional['datetime.datetime'] = None,
+                 initial_time: Optional[Union['datetime.datetime', str]] = None,
                  irc_direction: Optional[str] = None,
                  job_id: Optional[int] = None,
                  job_memory_gb: float = 14.0,
                  job_name: Optional[str] = None,
                  job_num: Optional[int] = None,
                  job_status: Optional[List[Union[dict, str]]] = None,
+                 level: Optional['Level'] = None,
                  max_job_time: Optional[float] = None,
                  reactions: Optional[List['ARCReaction']] = None,
                  rotor_index: Optional[int] = None,
                  server: Optional[str] = None,
                  server_nodes: Optional[list] = None,
                  species: Optional[List['ARCSpecies']] = None,
-                 tasks: Optional[int] = None,
                  testing: bool = False,
                  torsions: List[List[int]] = None,
                  tsg: Optional[int] = None,
@@ -164,7 +162,6 @@ class GaussianAdapter(JobAdapter):
             os.makedirs(self.project_directory)
         self.job_types = job_type if isinstance(job_type, list) else [job_type]  # always a list
         self.job_type = job_type if isinstance(job_type, str) else job_type[0]  # always a string
-        self.level = level
         self.args = args or dict()
         self.bath_gas = bath_gas
         self.checkfile = checkfile
@@ -184,17 +181,17 @@ class GaussianAdapter(JobAdapter):
         self.job_num = job_num
         self.job_status = job_status \
             or ['initializing', {'status': 'initializing', 'keywords': list(), 'error': '', 'line': ''}]
+        self.level = level
         self.max_job_time = max_job_time or default_job_settings.get('job_time_limit_hrs', 120)
         self.reactions = [reactions] if not isinstance(reactions, list) else reactions
         self.rotor_index = rotor_index
         self.server = server
         self.server_nodes = server_nodes or list()
         self.species = [species] if not isinstance(species, list) else species
-        self.tasks = tasks
         self.testing = testing
         self.torsions = torsions
         self.tsg = tsg
-        self.xyz = xyz
+        self.xyz = xyz or self.species[0].get_xyz()
 
         if self.job_num is None:
             self._set_job_number()
@@ -263,7 +260,7 @@ class GaussianAdapter(JobAdapter):
         input_dict['method'] = self.level.method
         input_dict['multiplicity'] = self.multiplicity
         input_dict['scan_trsh'] = self.args['trsh']['scan_trsh'] if 'scan_trsh' in self.args['trsh'] else ''
-        input_dict['xyz'] = xyz_to_str(self.xyz or self.species[0].get_xyz())
+        input_dict['xyz'] = xyz_to_str(self.xyz)
 
         if self.level.basis is not None:
             input_dict['slash_1'] = '/'
