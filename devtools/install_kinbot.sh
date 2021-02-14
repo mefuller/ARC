1150a4ebffd0204e4fb4d44265d0c087eed43c4f
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-14 22:08:30 +0200

    f install scripts

--- devtools/install_kinbot.sh
+++ devtools/install_kinbot.sh
@@ -1,5 +1,6 @@
 # temporarily change directory to install software, and move one directory up in the tree
-pushd ..
+pushd .
+cd ..
 
 # clone the repo in the parent directory and update it
 echo "Cloning/Updating KinBot..."
