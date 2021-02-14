55410138cb800c3ca433a0d0640df0e61cce26aa
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-14 11:52:59 +0200

    qchem

--- arc/job/adapters/qchem.py
+++ arc/job/adapters/qchem.py
@@ -389,6 +389,7 @@ class QChemAdapter(JobAdapter):
               )
         self._log_job_execution()
         execute_command(incore_commands[self.server][self.job_adapter])
+        self.job_status[0] = 'done'
 
     def execute_queue(self):
         """
