2cb55acd8c7f9a67b0f8e079292632ca3f130428
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-15 08:47:09 +0200

    f! Allow job_id to be a string in local and ssh

--- arc/job/local.py
+++ arc/job/local.py
@@ -159,19 +159,23 @@ def check_running_jobs_ids() -> list:
     running_jobs_ids = list()
     cmd = check_status_command[servers['local']['cluster_soft']] + ' -u $USER'
     stdout = execute_command(cmd)[0]
+    job_id = None
     for i, status_line in enumerate(stdout):
         if servers['local']['cluster_soft'].lower() == 'slurm' and i > 0:
             job_id = status_line.split()[0]
+            break
         elif servers['local']['cluster_soft'].lower() == 'oge' and i > 1:
             job_id = status_line.split()[0]
+            break
         elif servers['local']['cluster_soft'].lower() == 'pbs' and i > 4:
             job_id = status_line.split('.')[0]
-        else:
-            raise ValueError(f"Server cluster software {servers['local']['cluster_soft']} is not supported.")
-        try:
-            job_id = int(job_id)
-        except ValueError:
-            pass
+            break
+    if job_id is None:
+        raise ValueError(f"Server cluster software {servers['local']['cluster_soft']} is not supported.")
+    try:
+        job_id = int(job_id)
+    except ValueError:
+        pass
         running_jobs_ids.append(job_id)
     return running_jobs_ids
 
