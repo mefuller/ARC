2920a0a9d99952502d7b0e43339ccaf84beb21bd
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-15 08:41:05 +0200

    Allow job_id to be a string in local and ssh
    In some cases it could be "9258224_1", which causes the int() operation to fail.

--- arc/job/local.py
+++ arc/job/local.py
@@ -152,7 +152,7 @@ def delete_job(job_id):
             logger.warning(f'Job {job_id} is no longer running, so we can continue.')
 
 
-def check_running_jobs_ids():
+def check_running_jobs_ids() -> list:
     """
     Return a list of ``int`` representing job IDs of all jobs submitted by the user on a server
     """
@@ -161,13 +161,18 @@ def check_running_jobs_ids():
     stdout = execute_command(cmd)[0]
     for i, status_line in enumerate(stdout):
         if servers['local']['cluster_soft'].lower() == 'slurm' and i > 0:
-            print(status_line)
-            running_jobs_ids.append(int(status_line.split()[0]))
+            job_id = status_line.split()[0]
         elif servers['local']['cluster_soft'].lower() == 'oge' and i > 1:
-            running_jobs_ids.append(int(status_line.split()[0]))
+            job_id = status_line.split()[0]
         elif servers['local']['cluster_soft'].lower() == 'pbs' and i > 4:
-            running_jobs_ids.append(int(status_line.split('.')[0]))
-
+            job_id = status_line.split('.')[0]
+        else:
+            raise ValueError(f"Server cluster software {servers['local']['cluster_soft']} is not supported.")
+        try:
+            job_id = int(job_id)
+        except ValueError:
+            pass
+        running_jobs_ids.append(job_id)
     return running_jobs_ids
 
 
