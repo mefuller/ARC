224b622296d2a2acac55c8ea0b7d99d21c56b692
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-16 13:00:14 +0200

    autotst

--- arc/job/adapters/ts/autotst_ts.py
+++ arc/job/adapters/ts/autotst_ts.py
@@ -179,6 +179,9 @@ class AutoTSTAdapter(JobAdapter):
 
         self.final_time = None
         self.run_time = None
+        self.charge = None
+        self.multiplicity = None
+        self.is_ts = True
         self.scan_res = None
         self.set_file_paths()
 
