d9837384a6f9cba9d2e55c89a71b05b37dbf058b
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-16 16:20:04 +0200

    autotst

--- arc/job/adapters/ts/autotst_ts.py
+++ arc/job/adapters/ts/autotst_ts.py
@@ -304,6 +304,7 @@ class AutoTSTAdapter(JobAdapter):
 
         self.final_time = datetime.datetime.now()
         self.job_status[0] = 'done'
+        self.job_status[1]['status'] = 'done'
 
     def execute_queue(self):
         """
