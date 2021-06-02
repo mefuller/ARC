21b7fa60050d5c9507486f103a1301d58cfb550f
Alon Grinberg Dana <alon@technion.ac.il>
2021-06-02 11:26:47 +0300

    TS-GCN py geo 1.8.0

---
+++ devtools/create_env_cpu.sh
@@ -0,0 +1,53 @@
+# This script does the following tasks:
+# 	- creates the conda environment
+# 	- installs PyTorch cpu version (i.e. CUDA None) https://pytorch.org/get-started/locally/
+# 	- installs torch-geometric and the required dependencies in the environment: https://pytorch-geometric.readthedocs.io/en/latest/notes/installation.html
+
+
+# get OS type
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=MacOS;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo "Running ${machine}..."
+
+# "cpuonly" works for Linux and Windows
+CUDA="cpuonly"
+# Mac does not use "cpuonly"
+if [ $machine == "Mac" ]
+then
+    CUDA=" "
+fi  
+CUDA_VERSION="cpu"
+
+echo "Creating conda environment..."
+echo "Running: conda env create -f cpu_environment.yml"
+conda env create -f cpu_environment.yml
+
+# activate the environment to install torch-geometric
+echo "Checking which python"
+which python
+export PATH=$CONDA_PREFIX/bin:$PATH
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+echo "Activating ts_gcn environment"
+conda activate ts_gcn
+echo "Checking which python"
+which python
+
+echo "Installing torch-geometric..."
+echo "Using CUDA version: $CUDA_VERSION"
+# get PyTorch version
+TORCH_VERSION=1.8.0
+echo "Using PyTorch version: $TORCH_VERSION"
+echo "Installing PyTorch Geometric version: $TORCH_VERSION"
+
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.8.0+cpu.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.8.0+cpu.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.8.0+cpu.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.8.0+cpu.html
+pip install torch-geometric
--- devtools/install_gcn_cpu.sh
+++ devtools/install_gcn_cpu.sh
@@ -5,7 +5,7 @@ pushd ..
 git clone https://github.com/ReactionMechanismGenerator/TS-GCN
 
 # create the environment
-cd TS-GCN || exit
+cd ARC || exit
 cd devtools || exit
 bash create_env_cpu.sh
 
