53eabddf71ae1bf84acb1cb37ff6f35909e1b409
Alon Grinberg Dana <alon@technion.ac.il>
2021-06-06 22:04:26 +0300

    TMP

--- arc/settings/settings.py
+++ arc/settings/settings.py
@@ -178,7 +178,7 @@ valid_chars = "-_[]=.,%s%s" % (string.ascii_letters, string.digits)
 
 # A scan with better resolution (lower number here) takes more time to compute,
 # but the automatically-derived rotor symmetry number is more likely to be correct.
-rotor_scan_resolution = 30  # degrees. Default: 8.0
+rotor_scan_resolution = 10  # degrees. Default: 8.0
 
 # rotor validation parameters
 maximum_barrier = 40    # a rotor threshold (kJ/mol) above which the mode will be considered as vibrational if
