f5eafa50137709f74b10e667cca714e6a473bea1
Alon Grinberg Dana <alon@technion.ac.il>
2021-06-13 21:49:04 +0300

    f! common tests

--- arc/commonTest.py
+++ arc/commonTest.py
@@ -19,7 +19,7 @@ import arc.common as common
 import arc.species.converter as converter
 from arc.exceptions import InputError, SettingsError
 from arc.imports import settings
-from arc.parser import parse_normal_modes_displacement
+from arc.parser import parse_normal_mode_displacement
 
 
 servers = settings['servers']
@@ -714,8 +714,8 @@ H       1.98414750   -0.79355889   -0.24492049"""  # colliding atoms
     def test_get_rms_from_normal_modes_disp(self):
         """Test the get_rms_from_normal_modes_disp() function"""
         path_1 = os.path.join(common.ARC_PATH, 'arc', 'testing', 'freq', 'C3H7_intra_h_TS.out')
-        normal_modes_disp = parse_normal_modes_displacement(path_1)[1]
-        rms = common.get_rms_from_normal_modes_disp(normal_modes_disp)
+        normal_modes_disp = parse_normal_mode_displacement(path_1)[1]
+        rms = common.get_rms_from_normal_mode_disp(normal_modes_disp)
         self.assertEqual(rms, [0.07874007874011811,
                                0.07280109889280519,
                                0.0,
